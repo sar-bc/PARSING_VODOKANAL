0616264fdf098272fbb1f2a02f0ad6e9e3fb2cf8
Виктор <sar-bc@mail.ru>
2024-06-01 08:56:33 +0400

    add

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello")
+from bs4 import BeautifulSoup
+
+
+f = open("html_txt/otkl_1.html").read()
+soup = BeautifulSoup(f, "html.parser")
+row = soup.find_all('div', id=lambda x: x and x.startswith('nowaterdiv_'))
+print(row)
+
