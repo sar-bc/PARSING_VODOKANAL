fb3fd8fc148adea2276c7553538e1f4b2d65f861
Виктор <sar-bc@mail.ru>
2024-06-01 23:44:24 +0400

    add

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from bs4 import BeautifulSoup
+import requests
+URL = "https://engels-vodokanal.ru/index.php?show=water"
+address = [
+    "Персидского",
+    "Пристанская"
+]
+resp = requests.get(URL)
 
-
-f = open("html_txt/otkl_1.html").read()
-soup = BeautifulSoup(f, "html.parser")
-row = soup.find_all('div', id=lambda x: x and x.startswith('nowaterdiv_'))
-print(row)
+# f = open("html_txt/otkl_2.html").read()
+# soup = BeautifulSoup(f, "html.parser")
+soup = BeautifulSoup(resp.text, "html.parser")
+row = soup.find('div', id=lambda x: x and x.startswith('nowaterdiv_'))
+if row:
+    # print(row[0].text.strip())
+    print(row.text.strip())
+    for a in address:
+        if a in row.text:
+            print("Наш адрес отключат")
+else:
+    print("Нет отключений")
 
