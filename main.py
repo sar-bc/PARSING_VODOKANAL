fd9b0623ff81dac53cce73d57ca83fd8b65b5ee6
sar-bc <sar-bc@mail.ru>
2024-06-02 16:24:33 +0400

    add

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 from bs4 import BeautifulSoup
 import requests
+
 URL = "https://engels-vodokanal.ru/index.php?show=water"
 address = [
     "Персидского",
-    "Пристанская"
+    "Пристанская",
+    "Приволжский",
+    "Гагарина"
 ]
-resp = requests.get(URL)
 
-# f = open("html_txt/otkl_2.html").read()
-# soup = BeautifulSoup(f, "html.parser")
-soup = BeautifulSoup(resp.text, "html.parser")
-row = soup.find('div', id=lambda x: x and x.startswith('nowaterdiv_'))
-if row:
-    # print(row[0].text.strip())
-    print(row.text.strip())
-    for a in address:
-        if a in row.text:
-            print("Наш адрес отключат")
-else:
-    print("Нет отключений")
 
+def pars():
+    soup = BeautifulSoup(resp.text, "html.parser")
+    row = soup.find('div', id=lambda x: x and x.startswith('nowaterdiv_'))
+    if row:
+        # print(row[0].text.strip())
+        print(row.text.strip())
+        for a in address:
+            if a in row.text:
+                print("Наш адрес отключат")
+    else:
+        print("Нет отключений")
+
+
+resp = None
+try:
+    resp = requests.get(URL)
+
+    if resp.status_code == 200:
+        pars()
+    else:
+        print("Страница не найдена")
+
+except requests.exceptions.ConnectionError:
+    print('Seems like dns lookup failed..')
+except requests.exceptions.ReadTimeout:
+    print('Oops. Read timeout occured')
+# except requests.exceptions.ConnectTimeout:
+#     print('Oops. Connection timeout occured!')
